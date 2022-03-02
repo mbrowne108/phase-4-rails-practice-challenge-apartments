da4aae376c6a6c62dfd4c5ae998a4a464fbbfc25
Matt <mbrowne108@gmail.com>
2022-03-01 20:30:22 -0500

    Add all deliverables

---
+++ app/controllers/apartments_controller.rb
@@ -0,0 +1,48 @@
+class ApartmentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+        render json: Apartment.all
+    end
+
+    def show
+        apartment = find_apartment
+        render json: apartment
+    end
+
+    def create
+        apartment = Apartment.create!(apartment_params)
+        render json: apartment, status: :created
+    end
+
+    def update
+        apartment = find_apartment
+        apartment.update(apartment_params)
+        render json: apartment
+    end
+    
+    def destroy
+        apartment = find_apartment
+        apartment.destroy
+        head :no_content
+    end
+
+    private
+
+    def apartment_params
+        params.permit(:number)
+    end
+
+    def find_apartment
+        Apartment.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Camper not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
